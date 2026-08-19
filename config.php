fe97cac0dfdc01ee04df9f62e536f6c406977986
Emillycode <lima7emilly12@gmail.com>
2026-08-19 11:14:38 -0300

    atualizando banco de dados para rodar no render

--- config.php
+++ config.php
@@ -1,16 +1,16 @@
 <?php
 
-$hostname = 'localhost';
-$usuario = 'root';
-$senha = '';
-$bancodedados = 'cadastro';
+$hostname     = getenv('DB_HOST')     ?: 'localhost';
+$usuario      = getenv('DB_USER')     ?: 'root';
+$senha        = getenv('DB_PASSWORD') ?: '';
+$bancodedados = getenv('DB_NAME')     ?: 'cadastro';
+$porta        = getenv('DB_PORT')     ?: 3306;
 
-$conexao = new mysqli($hostname, $usuario, $senha, $bancodedados);
+$conexao = new mysqli($hostname, $usuario, $senha, $bancodedados, (int)$porta);
 
 if ($conexao->connect_error) {
     die("Falha na conexão: " . $conexao->connect_error);
 } 
 
 $conexao->set_charset("utf8");
-
 ?>
